cb42dd537cf209a0764437048666655812be9d3d
Avinash Patil <ak9011656290@gmail.com>
2025-04-02 23:52:10 +0530

    Add another student

--- src/main/java/com/springcore/springframework/App.java
+++ src/main/java/com/springcore/springframework/App.java
@@ -12,9 +12,12 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/springframework/config.xml");
-        
+        	
         Student student1 =  (Student) context.getBean("std1");
         System.out.println(student1);
+        
+        Student student2 = (Student) context.getBean("std2");
+        System.out.println(student2);
         
         
         
